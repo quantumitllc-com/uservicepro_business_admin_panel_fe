67fedce490b19927d592164a92e523411a72f921
Otabek <ota_2411@mail.ru>
2022-07-27 14:13:56 +0500

    test husky

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,11 @@
 import { Routing } from "pages"
 import { withProviders } from "./providers"
+
 import "./index.scss"
 
 const App = () => {
+    const tempVar = 5
+
     return (
         <div className="app">
             <Routing />
