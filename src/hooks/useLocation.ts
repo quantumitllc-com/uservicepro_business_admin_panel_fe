3a0956cf287d26c215d4e0e57f845c17329e1c37
bekzod9936 <bekzodnorkulov9936@gmail.com>
2023-02-16 21:04:13 +0500

    feat: react-select

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,13 +1,13 @@
-import { useQuery } from "@tanstack/react-query"
-import { getLocationList } from "services/dashboard/employee"
+import { useQuery } from "@tanstack/react-query";
+import { getLocationList } from "services/dashboard/employee";
 
 export const useLocation = () => {
-	const {
-		data: locationData = { content: [] },
-		isLoading: isLoadingLocation,
-	} = useQuery(["service-locations"], getLocationList, {
-		select: ({ data, ...rest }) => ({ ...rest, ...data }),
-	})
+  const {
+    data: locationData = { content: [] },
+    isLoading: isLoadingLocation,
+  } = useQuery(["service-locations"], getLocationList, {
+    select: ({ data, ...rest }) => ({ ...rest, ...data }),
+  });
 
-	return { dataLocation: locationData.content, isLoadingLocation }
-}
+  return { dataLocation: locationData.content, isLoadingLocation };
+};
