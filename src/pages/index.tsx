eb00e6c6f51f17c3ee70971b2deab2fa1b65421e
bekzod9936 <bekzodnorkulov9936@gmail.com>
2023-03-06 19:01:17 +0500

    some

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,7 @@ import { getTokens } from "../utils/getTokens"
 
 // dashboard
 const ActivityMap = lazy(() => import("./dashboard/activity-map"))
-const NewActivityMap = lazy(() => import("./dashboard/new-activity-map"))
-const Map = lazy(() => import("./dashboard/new-activity-map/map"))
+const Map = lazy(() => import("./dashboard/activity-map/map"))
 const Schedules = lazy(() => import("./dashboard/schedules"))
 
 // offices
@@ -121,8 +120,7 @@ export function Routing() {
 				<Route path="services" element={<Services />} />
 				<Route path="services/add" element={<AddServices />} />
 				<Route path="statistics" element={<Statistics />} />
-				<Route path="activity-map" element={<ActivityMap />} />
-				<Route path="new-activity-map" element={<NewActivityMap />}>
+				<Route path="activity-map" element={<ActivityMap />}>
 					<Route index element={<Map />} />
 				</Route>
 				<Route path="schedules" element={<Schedules />} />
