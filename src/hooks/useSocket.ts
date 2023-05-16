1b24be6408842529ae5968a2180f03c690174746
bekzod9936 <bekzodnorkulov9936@gmail.com>
2023-05-16 20:44:09 +0500

    fix: socket path

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,49 +1,50 @@
-import { io, Socket } from "socket.io-client"
-import { useEffect, useState } from "react"
+import { io, Socket } from "socket.io-client";
+import { useEffect, useState } from "react";
 
-import { useNavigate } from "react-router-dom"
-import { isExpiredToken } from "../utils/isExpiredToken"
-import { clearStorage } from "../utils/clearStorage"
-import { refreshToken } from "../utils/refreshToken"
+import { useNavigate } from "react-router-dom";
+import { isExpiredToken } from "../utils/isExpiredToken";
+import { clearStorage } from "../utils/clearStorage";
+import { refreshToken } from "../utils/refreshToken";
 
 export const useSocket = () => {
-	const tokens = isExpiredToken()
-	const [socket, setSocket] = useState<Socket | null>(null)
-	const navigate = useNavigate()
+  const tokens = isExpiredToken();
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		if (!tokens.isExpiredAccess) {
-			const newSocket = io("https://gateway.uservicepro.com", {
-				extraHeaders: {
-					Authorization: `Bearer ${tokens.accessToken}`,
-				},
-				transports: ["polling"], // Use HTTP long-polling instead of WebSocket
-				autoConnect: false,
-			})
+  useEffect(() => {
+    if (!tokens.isExpiredAccess) {
+      const newSocket = io("https://gateway.uservicepro.com", {
+        extraHeaders: {
+          Authorization: `Bearer ${tokens.accessToken}`,
+        },
+        path: "/chat/socket.io",
+        transports: ["polling"], // Use HTTP long-polling instead of WebSocket
+        autoConnect: false,
+      });
 
-			setSocket(newSocket)
-		}
+      setSocket(newSocket);
+    }
 
-		if (tokens.isExpiredAccess) {
-			;(async () => {
-				await refreshToken()
-			})()
-		}
+    if (tokens.isExpiredAccess) {
+      (async () => {
+        await refreshToken();
+      })();
+    }
 
-		if (tokens.isExpiredRefresh) {
-			clearStorage()
-			navigate("/sign-in")
-		}
+    if (tokens.isExpiredRefresh) {
+      clearStorage();
+      navigate("/sign-in");
+    }
 
-		return () => {
-			socket?.close()
-		}
-	}, [
-		setSocket,
-		tokens.accessToken,
-		tokens.isExpiredRefresh,
-		tokens.isExpiredAccess,
-	])
+    return () => {
+      socket?.close();
+    };
+  }, [
+    setSocket,
+    tokens.accessToken,
+    tokens.isExpiredRefresh,
+    tokens.isExpiredAccess,
+  ]);
 
-	return socket
-}
+  return socket;
+};
--- src/services/dashboard/chat/index.ts
+++ src/services/dashboard/chat/index.ts
@@ -1,4 +1,4 @@
-import { request } from "http/"
+import { request } from "http/";
 
 export const getMessages = <T>(params: T) =>
-	request.get("chat/messages", { params })
+  request.get("chat/api/messages", { params });
